0aaf47ccfb11f74f82134e2eae76dd8ef6d6f488
yasirabd <sir.yasirabd@gmail.com>
2018-02-27 09:17:58 +0700

    ch 4: database

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'whatever-you-think'
+    TEMPLATES_AUTO_RELOAD = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
