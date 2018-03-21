4991eaafb05cfb3ad9c0d4f0ad41ffa6964b6d39
yasirabd <sir.yasirabd@gmail.com>
2018-03-21 14:26:05 +0700

    ch14: ajax (failed to translate post)

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,24 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for, request, g
-from app.forms import LoginForm, EditProfileForm, RegistrationForm, \
-    PostForm, ResetPasswordRequestForm, ResetPasswordForm
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
+    ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app.email import send_password_reset_email
 from flask_babel import _, get_locale
+from guess_language import guess_language
+from app.translate import translate
+
 
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    g.locale = str(get_locale())
+    # g.locale = str(get_locale())
+    g.locale = 'id'
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -22,7 +26,10 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -174,3 +181,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
